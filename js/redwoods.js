5320a240d756821f95136411faf72b8b6733f7ee
gobeklitech <102619811+gobeklitech@users.noreply.github.com>
2022-06-03 19:37:34 -0700

    Update

---
+++ js/redwoods.js
@@ -0,0 +1,125 @@
+// JavaScript for event and content management for Redwoods Biome page
+
+function redwoodsInit(){
+
+	$("#deer-div").hide(20);
+	$("#bay-div").hide(20);
+	$("#coyote-div").hide(20);
+	$("#foamflower-div").hide(20);
+	$("#poison-oak-div").hide(20);
+	$("#woodpecker-div").hide(20);
+
+	var audio = new Audio("../aud/crickets.mp3");
+
+	audio.currenttime = 0;
+
+	audio.play();
+
+};
+
+
+function deerShow(){
+
+	$("#deer-div").show(800);
+
+};
+
+//
+
+function deerHide(){
+
+	$("#deer-div").hide(800);
+
+};
+
+//
+
+function bayShow(){
+
+	$("#bay-div").show(800);
+
+};
+
+//
+
+function bayHide(){
+
+	$("#bay-div").hide(800);
+
+};
+
+//
+
+function coyoteShow(){
+
+	$("#coyote-div").show(800);
+
+};
+
+//
+
+function coyoteHide(){
+
+	$("#coyote-div").hide(800);
+
+};
+
+//
+
+function foamflowerShow(){
+
+	$("#foamflower-div").show(800);
+
+};
+
+function foamflowerHide(){
+
+	$("#foamflower-div").hide(800);
+
+};
+
+//
+
+function poisonoakShow(){
+
+	$("#poison-oak-div").show(800);
+
+};
+
+function poisonoakHide(){
+
+	$("#poison-oak-div").hide(800);
+
+};
+
+//
+
+function woodpeckerShow(){
+
+	$("#woodpecker-div").show(800);
+
+};
+
+function woodpeckerHide(){
+
+	$("#woodpecker-div").hide(800);
+
+};
+
+//
+
+$("#woodpecker-map").hover(woodpeckerShow,woodpeckerHide);
+
+$("#coyote-map").hover(coyoteShow,coyoteHide);
+
+$("#foamflower-map").hover(foamflowerShow,foamflowerHide);
+
+$("#poison-oak-map").hover(poisonoakShow,poisonoakHide);
+
+$("#bay-map").hover(bayShow,bayHide);
+
+$("#deer-map").hover(deerShow,deerHide);
+
+
+
+
--- js/sandhill.js
+++ js/sandhill.js
@@ -1,27 +1,20 @@
-// JavaScript for event and content management for Wetlands Biome page
+// JavaScript for event and content management for Sandhill Biome page
 
 function sandhillInit(){
 
-	// $("#content-div").innerHTML = "<h1>Water Fowl of Santa Cruz</h1><hr><p>Waterfowl are all types of ducks, geese, and swans. They are mid-sized to large birds most commonly found on or near water. Most have plump bodies, long necks, short wings, and all have webbed feet. Geese tend to be larger than ducks with shorter bills and swans are even bigger with long necks and are usually white.Male ducks are typically more colorful than females during the breeding season.</p><h3>Interesting Fact:</h3><p> Most waterfowl fly at speeds of 40 to 60 mph!</p><h5>Works Cited:</h5><p>Paws. “Waterfowl.” PAWS, <a href="url">www.paws.org/resources/waterfowl/.<a>“Waterfowl Habitat.” World Leader in Wetlands & Waterfowl Conservation, Wetland Conservation, <a href="url">www.ducks.org/conservation/waterfowl-habitat</a></p>";
-	//$("#content-div").innerHTML = $("testname");
 	$("#buckwheat-div").hide(20);
 	$("#rat-div").hide(20);
 	$("#manzanita-div").hide(20);
 	$("#spineflower-div").hide(20);
 
+	var audio = new Audio("../aud/crickets.mp3");
+
+	audio.currenttime = 0;
+
+	audio.play();
+
 };
 
-// function contentShow(){
-
-// 	$("#testname").show(800);
-
-// };
-
-// function contentHide(){
-
-// 	$("#testname").hide(800);
-
-// };
 
 function buckwheatShow(){
 
@@ -73,41 +66,26 @@ function manzanitaHide(){
 
 function spineflowerShow(){
 
-	$("#frog-div").show(800);
+	$("#spineflower-div").show(800);
 
 };
 
 function spineflowerHide(){
 
-	$("#frog-div").hide(800);
+	$("#spineflower-div").hide(800);
 
 };
 
 //
 
-// function rushShow(){
+$("#buckwheat-map").hover(buckwheatShow,buckwheatHide);
 
-// 	$("#rush-div").show(800);
+$("#rat-map").hover(ratShow,ratHide);
 
-// };
+$("#manzanita-map").hover(manzanitaShow,manzanitaHide);
 
-// function rushHide(){
+$("#spineflower-map").hover(spineflowerShow,spineflowerHide);
 
-// 	$("#rush-div").hide(800);
-
-// };
-
-//
-
-$("#tule-map").hover(tuleShow,tuleHide);
-
-$("#waterfowl-map").hover(waterfowlShow,waterfowlHide);
-
-$("#willow-map").hover(willowShow,willowHide);
-
-$("#frog-map").hover(frogShow,frogHide);
-
-$("#rush-map").hover(rushShow,rushHide);
 
 
 
