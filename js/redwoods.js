f79b44d951c614f1c3c808a970e51f9b16069ce8
gobeklitech <102619811+gobeklitech@users.noreply.github.com>
2022-06-06 23:13:52 -0700

    Ready to deploy

--- js/redwoods.js
+++ js/redwoods.js
@@ -1,5 +1,73 @@
 // JavaScript for event and content management for Redwoods Biome page
 
+
+function getRedwoodsWeather(){
+
+	let weather = "temp";
+
+	const getTimelineURL = "https://api.tomorrow.io/v4/timelines";
+
+	const apikey = "XQi7VX8xGL3waqWqXgz1sLSg6HU7xHbQ";
+
+	const fields = ["temperature","windSpeed"];
+
+	const timesteps = ["current"];
+
+	const units = "imperial";
+
+	const location = "37.021,-122.047"
+
+	$.ajax({
+	    
+	    url: getTimelineURL,
+	    
+	    data: { 
+	            apikey: apikey,
+	            fields: fields,
+	            timesteps: timesteps,
+	            location: location,
+	            units: units
+	          },
+
+	    type: "GET",
+
+	    dataType : "json",
+
+	    success: function(data) {
+
+	        console.log("I've Succeeded!");
+
+	        console.log(data);
+
+	        let current_temp = data.data.timelines[0].intervals[0].values.temperature;
+
+	        let current_wind_speed = data.data.timelines[0].intervals[0].values.windSpeed;
+
+	        console.log("Temperature: " + current_temp);
+
+	        console.log("Wind Speed: " + current_wind_speed);
+
+	        $("#output-weather").append("<strong>Current Temperature: " + current_temp + " degrees Fahrenheit.</strong>");
+
+	        $("#output-weather").append("<br><br>");
+
+	       	$("#output-weather").append("<strong>Current Wind Speed: " + current_wind_speed + " miles per hour.</strong><br>");
+
+	    },
+
+	    error: function (jqXHR, textStatus, errorThrown) { 
+
+	    	console.log("I've Failed..");
+
+	        console.log("Error:", textStatus, errorThrown);
+	    }
+	})
+
+}
+
+
+
+
 function redwoodsInit(){
 
 	$("#deer-div").hide(20);
@@ -8,6 +76,8 @@ function redwoodsInit(){
 	$("#foamflower-div").hide(20);
 	$("#poison-oak-div").hide(20);
 	$("#woodpecker-div").hide(20);
+
+	getRedwoodsWeather();
 
 };
 
--- js/wetlands.js
+++ js/wetlands.js
@@ -1,5 +1,70 @@
 // JavaScript for event and content management for Wetlands Biome page
 
+function getWetlandsWeather(){
+
+	let weather = "temp";
+
+	const getTimelineURL = "https://api.tomorrow.io/v4/timelines";
+
+	const apikey = "XQi7VX8xGL3waqWqXgz1sLSg6HU7xHbQ";
+
+	const fields = ["temperature","windSpeed"];
+
+	const timesteps = ["current"];
+
+	const units = "imperial";
+
+	const location = "36.967,-122.124"
+
+	$.ajax({
+	    
+	    url: getTimelineURL,
+	    
+	    data: { 
+	            apikey: apikey,
+	            fields: fields,
+	            timesteps: timesteps,
+	            location: location,
+	            units: units
+	          },
+
+	    type: "GET",
+
+	    dataType : "json",
+
+	    success: function(data) {
+
+	        console.log("I've Succeeded!");
+
+	        console.log(data);
+
+	        let current_temp = data.data.timelines[0].intervals[0].values.temperature;
+
+	        let current_wind_speed = data.data.timelines[0].intervals[0].values.windSpeed;
+
+	        console.log("Temperature: " + current_temp);
+
+	        console.log("Wind Speed: " + current_wind_speed);
+
+	        $("#output-weather").append("<strong>Current Temperature: " + current_temp + " degrees Fahrenheit.</strong>");
+
+	        $("#output-weather").append("<br><br>");
+
+	       	$("#output-weather").append("<strong>Current Wind Speed: " + current_wind_speed + " miles per hour.</strong><br>");
+
+	    },
+
+	    error: function (jqXHR, textStatus, errorThrown) { 
+
+	    	console.log("I've Failed..");
+
+	        console.log("Error:", textStatus, errorThrown);
+	    }
+	})
+
+}
+
+
 function wetlandsInit(){
            
 	$("#tule-div").hide(20);
@@ -7,6 +72,8 @@ function wetlandsInit(){
 	$("#rush-div").hide(20);
 	$("#frog-div").hide(20);
 	$("#willow-div").hide(20);
+
+	getWetlandsWeather();
 
 };
 
